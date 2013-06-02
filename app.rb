e73258d85c1ea7b63d63115cb569cb68a423e023
Sam Newman <sam.newman@gmail.com>
2013-06-02 10:55:21 +1000

    Added json feed for compare page death data

--- app.rb
+++ app.rb
@@ -67,6 +67,53 @@ get '/compare/:firstslug/vs/:secondslug' do | firstslug, secondslug |
   haml :compare, locals: { first: Card.where(slug:firstslug).first, second: Card.where(slug:secondslug).first }
 end
 
+#
+#card.data_points.reverse.each do | dp |
+#  %tr
+#  %td= dp.year
+#  %td.center= dp.males
+#  %td.center= dp.females
+#  %td.center= dp.persons
+
+
+#sample = [
+#    {
+#        "color": "blue",
+#    "name": "New York",
+#    "data": [ { "x": 2001, "y": 40 }, { "x": 1, "y": 49 }, { "x": 2, "y": 38 }, { "x": 3, "y": 30 }, { "x": 4, "y": 32 } ]
+#}, {
+#    "name": "London",
+#    "data": [ { "x": 0, "y": 19 }, { "x": 1, "y": 22 }, { "x": 2, "y": 29 }, { "x": 3, "y": 20 }, { "x": 4, "y": 14 } ]
+#}, {
+#    "name": "Tokyo",
+#    "data": [ { "x": 0, "y": 8 }, { "x": 1, "y": 12 }, { "x": 2, "y": 15 }, { "x": 3, "y": 11 }, { "x": 4, "y": 10 } ]
+#}
+#]
+#
+get '/compare/:firstslug/vs/:secondslug/json' do | firstslug, secondslug |
+  require 'json'
+  content_type :json
+  first = Card.where(slug:firstslug).first
+  first_by_year = []
+  first.data_points.reverse.each do | dp |
+    first_by_year << { :year => dp.year, :deaths => dp.persons }
+  end
+
+  second = Card.where(slug:secondslug).first
+  second_by_year = []
+  second.data_points.reverse.each do | dp |
+    second_by_year << { :year => dp.year, :deaths => dp.persons }
+  end
+
+  return [
+      {:name => first.title,
+       :data => first_by_year},
+      {:name => second.title,
+       :data => second_by_year}
+  ].to_json
+
+end
+
 get '/stylesheets/:stylesheet.css' do |stylesheet|
   scss :"stylesheets/#{stylesheet}"
 end
